f7e15c05c544b52b48fba02634d9645a369c99fa
Herberto Graça <herberto.graca@gmail.com>
2013-04-22 14:26:29 +0200

    Changed gitignore to only ignore the project and workspace files of sublime

--- sublime-text-2-php-refactor.py
+++ sublime-text-2-php-refactor.py
@@ -35,7 +35,7 @@ Prefs.load()
 
 
 '''
-Top level class for the plugin commands
+    Top level class for the plugin commands
 '''
 
 
@@ -93,11 +93,11 @@ class Refactor():
 
 
 '''
-Extract a range of lines into a new method and call this method from the original location.
-This refactoring automatically detects all necessary inputs and outputs from the function and generates the argument list and return statement accordingly.
-Sintax of the external command:
-    php refactor.phar extract-method <file> <line-range> <new-method>|colordiff
-    php refactor.phar extract-method <file> <line-range> <new-method>|patch -b <file>
+    Extract a range of lines into a new method and call this method from the original location.
+    This refactoring automatically detects all necessary inputs and outputs from the function and generates the argument list and return statement accordingly.
+    Sintax of the external command:
+        php refactor.phar extract-method <file> <line-range> <new-method>|colordiff
+        php refactor.phar extract-method <file> <line-range> <new-method>|patch -b <file>
 '''
 
 
@@ -129,9 +129,9 @@ class ExtractCommand(sublime_plugin.TextCommand, Refactor):
 
 
 '''
-Rename a local variable from one to another name.
-Sintax of the external command:
-    php refactor.phar rename-local-variable <file> <line> <old-name> <new-name>
+    Rename a local variable from one to another name.
+    Sintax of the external command:
+        php refactor.phar rename-local-variable <file> <line> <old-name> <new-name>
 '''
 
 
@@ -141,9 +141,9 @@ class RenameLocalVariableCommand(sublime_plugin.TextCommand):
 
 
 '''
-Converts a local variable into an instance variable, creates the property and renames all the occurrences in the selected method to use the instance variable.
-Sintax of the external command:
-    php refactor.phar convert-local-to-instance-variable <file> <line> <variable>
+    Converts a local variable into an instance variable, creates the property and renames all the occurrences in the selected method to use the instance variable.
+    Sintax of the external command:
+        php refactor.phar convert-local-to-instance-variable <file> <line> <variable>
 '''
 
 
